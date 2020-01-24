857d192c25c45341a1a0abce51c2e5678e1080be
sweiland <hello@sweiland.at>
2020-01-24 10:05:02 +0100

    update everything

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -63,7 +63,6 @@ import {JwtModule} from '@auth0/angular-jwt';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttperrorInterceptor} from './httperror.interceptor';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {MatQuillModule} from '@webacad/ng-mat-quill';
 import {PasswordChangeComponent, UserFormComponent} from './user-form/user-form.component';
 import {BarRatingModule} from 'ngx-bar-rating-odilo';
 import {MatFaqComponent} from './faq/mat-faq/mat-faq.component';
@@ -148,7 +147,6 @@ export function tokenGetter() {
     MatExpansionModule,
     MatProgressBarModule,
     MatTooltipModule,
-    MatQuillModule,
     MatExpansionModule,
     NgxChartsModule,
     MatTabsModule,
